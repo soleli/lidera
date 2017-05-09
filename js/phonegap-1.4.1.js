53aded8c074a70690c9ff96849f87590a38b6735
Soledad Alaniz <sole_lr@hotmail.com>
2017-05-09 11:24:08 -0300

    correccion de error3

--- js/phonegap-1.4.1.js
+++ js/phonegap-1.4.1.js
@@ -4583,4 +4583,12 @@ PhoneGap.addConstructor(function() {
         PhoneGap.waitForInitialization("cupcakeStorage");
     }
 });
+
+{
+	"file": "plugins/com.lampa.startapp/www/startApp.manually.js",
+	"id": "com.lampa.startapp.startapp",
+	"merges": [
+		"startApp"
+	]
+}
 }
