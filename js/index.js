e19f466abef3163cd1ce6e52c70d87b084f30d7b
Soledad Alaniz <sole_lr@hotmail.com>
2017-05-11 11:36:33 -0300

    push prueba6

--- js/index.js
+++ js/index.js
@@ -35,9 +35,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+	alert("aqui2");
         var push = PushNotification.init({
-		alert("aqui2");
-            "android": {
+		    "android": {
                 "senderID": "275015233935"
             },
             "ios": {"alert": "true", "badge": "true", "sound": "true"}, 
