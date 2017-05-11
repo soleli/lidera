65bcf516e32ec616bddb541ea256fa67867f522c
Soledad Alaniz <sole_lr@hotmail.com>
2017-05-11 11:48:09 -0300

    push prueba7

--- js/index.js
+++ js/index.js
@@ -20,22 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
 	alert("aqui");
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-	alert("aqui1");
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-	alert("aqui2");
+     
         var push = PushNotification.init({
 		    "android": {
                 "senderID": "275015233935"
