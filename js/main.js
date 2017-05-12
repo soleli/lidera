14151597b7e8b6d9db894844cd065b8fbc42fabe
Soledad Alaniz <sole_lr@hotmail.com>
2017-05-12 13:05:38 -0300

    final

--- js/main.js
+++ js/main.js
@@ -1,63 +1,65 @@
-function redsocial(expr,id){ 
-    switch (expr) {
-      case 3:
-
-        window.plugins.socialsharing.shareViaTwitter('Mira esta noticia de Tiempo Popular', null /* img */, 'http://www.tiempopopular.com.ar/2014/ver_noticia_4S5As4ar2A.php?id_BWw1oeuRYF='+id);
-            
-        break;
-      case 2:
-        window.plugins.socialsharing.shareViaFacebook('Mira esta noticia de Tiempo Popular', null /* img */, 'http://www.tiempopopular.com.ar/2014/ver_noticia_4S5As4ar2A.php?id_BWw1oeuRYF='+id, function() {console.log('share ok')}, function(errormsg){console.log(errormsg)});
-           
-        break;
-      case 1:
-            window.plugins.socialsharing.shareViaWhatsApp('Mira esta noticia de Tiempo Popular', null /* img */, 'http://www.tiempopopular.com.ar/2014/ver_noticia_4S5As4ar2A.php?id_BWw1oeuRYF='+id, function() {console.log('share ok')}, function(errormsg){console.log(errormsg)});
-           
-        break;
-      default:
-        console.log(id);
-    }
-} 		
+	
 function init() 
 {
-	i=0;
-	archivoValidacion = "http://www.tiempopopular.com.ar/json/carusel.php?jsoncallback=?"
+
+	archivoValidacion = "http://www.wabes.com.ar/lidera/novedades.php?jsoncallback=?"
 
 
-	$.getJSON( archivoValidacion, {id:1})
+	$.getJSON( archivoValidacion)
 
 	.done(function(data2) {
 	
 	/*$("#cargando").css("display","none");*/
 		$(data2).each(function (index, data2) {
 	
-			if(i==0)
-				{
-					$(".carousel-inner").append(
-					
-						"<div class='item active'>"+
-						"<a href='#noticia1' data-transition='flip'  onclick='cargar("+data2.id_Noticia+");'>"+
-							"<img  src='http://tiempopopular.com.ar/2014/imagenes/"+data2.url+"_s.jpg'>"+
-							"<div class='carousel-caption'>"+
-									"<h1>"+data2.titulo+"</h1>"+
-							"</div> </a></div>"
-					);
-				}
-			else{
-					$(".carousel-inner").append(
-				
-						"<div class='item'>"+
-							"<a href='#noticia1' data-transition='flip'  onclick='cargar("+data2.id_Noticia+");'>"+
-						"<img  class='img-responsive' src='http://tiempopopular.com.ar/2014/imagenes/"+data2.url+"_s.jpg'>"+
-						"<div class='carousel-caption'>"+
-								"<h1>"+data2.titulo+"</h1>"+
-								
-						"</div></a></div>"
-					);
-				}
 			
-			i++;
+					$("#alerta_novedades").append(
+					"<button type='button' class='close' data-dismiss='alert' aria-label='Close' style='  width: 20%;    top: 0;'>"+
+					"<span aria-hidden='true' style='   font-size: 18px;'>×</span>"+
+				  "</button>"+
+						"<p>"+data2.mensaje+"</p>"+
+					"<a href='#novedades' onclick='novedades()'> Ver más</a>"
+					);
+				$("#alerta_novedades").css("display","block");
+			
 		});
-		$("#cargando").css("display","none");
+		
+	})
+	
+	.error( function(data2) { 
+	$("#cargando").html("");
+	$("#cargando").append("<p>Error al cargar. </p>"); 
+	$("#cargando").append("<p>Compruebe su conexion a internet.</p>"); 
+	$("#cargando").append("<button type='button' class='btn btn-success' onclick='init()'>Intertar de Nuevo</button>"); 
+	} )
+
+
+	
+	}
+		
+function novedades() 
+{
+ $(".novedades").html (" "); 
+	archivoValidacion = "http://www.wabes.com.ar/lidera/novedades_todas.php?jsoncallback=?"
+
+
+	$.getJSON( archivoValidacion)
+
+	.done(function(data2) {
+	
+	/*$("#cargando").css("display","none");*/
+		$(data2).each(function (index, data2) {
+	
+			
+					$(".novedades").append(
+					"<table class='table table-condensed'>"+  
+					"<td><p>"+data2.fecha+"</p>"+
+					"<td><p>"+data2.mensaje+"</p></td>"+
+					"</table>"
+					);
+				$("#alerta_novedades").css("display","block");
+			
+		});
 		
 	})
 	
@@ -76,8 +78,9 @@ function init()
              
              	var number = $("#nombre").val();
              	var message = $("#mensaje").val();
-				archivoValidacion = "http://tiempopopular.com.ar/json/formulario.php?jsoncallback=?"
-	$.getJSON( archivoValidacion, {nombre:number, mensaje:message})
+				var tel = $("#tel").val();
+				archivoValidacion = "http://wabes.com.ar/lidera/formulario.php?jsoncallback=?"
+	$.getJSON( archivoValidacion, {nombre:number, mensaje:message,tel:tel})
 	            .done(function(data) 
 	{
 	$("#alertaco").css("display","block");
